8d1dbf64bfb4fd8040d20a7e9a3562bc557bb1c0
Rafael Paladini <56520474+PaMeirelles@users.noreply.github.com>
2022-11-21 13:56:33 +0000

    struct + num_bytes

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+struct{
+  unsigned int address;
+  unsigned char r;
+  unsigned char w;
+  unsigned int acesso;
+}typedef s_quadro;
+
+
+unsigned int num_bytes(unsigned int size){
+  unsigned int i = 0;
+  while(size > 1){
+    i++;
+    size /= 2;
+  }
+  return 10 + i;
+}
+
+
 int main(void) {
+  printf("%d\n", num_bytes(8));
   printf("Hello World\n");
   return 0;
 }
