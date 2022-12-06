8f5e16a6ae6a9e7a60470643a477a589aae65cb7
rsgmello-felipe <91546708+rsgmello-felipe@users.noreply.github.com>
2022-12-05 22:30:49 -0300

    added new functions

--- simulador.c
+++ simulador.c
@@ -81,11 +81,91 @@ void insert(s_hash_table * table, unsigned int key){
     }
 }
 
+void handle_collision(s_hash_table* table, unsigned long index, s_info_endereco* item) {
+    LinkedList* head = table->overflow_buckets[index];
+
+    if (head == NULL) {
+        // We need to create the list
+        head = allocate_list();
+        head->item = item;
+        table->overflow_buckets[index] = head;
+        return;
+    }
+    else {
+        // Insert to the list
+        table->overflow_buckets[index] = linkedlist_insert(head, item);
+        return;
+    }
+ }
+
 // Caso a chave já exista, apenas chama update
 // Caso não exista, chama insert e então update
 // Uma função auxiliar para procurar na hashtable pode ser necessária
 void novo_uso(s_hash_table * table, unsigned int key, int linha_novo_uso);
-void delete(s_hash_table * table, unsigned int key);
+
+
+void delete(s_hash_table * table, unsigned int key){
+    // Deletes an item from the table
+    int index = hash_function(key);
+    Ht_item* item = table->items[index];
+    LinkedList* head = table->overflow_buckets[index];
+
+    if (item == NULL) {
+        // Does not exist. Return
+        return;
+    }
+    else {
+        if (head == NULL && strcmp(item->key, key) == 0) {
+            // No collision chain. Remove the item
+            // and set table index to NULL
+            table->itens[index] = NULL;
+            free_item(item);
+            table->count--;
+            return;
+        }
+        else if (head != NULL) {
+            // Collision Chain exists
+            if (strcmp(item->key, key) == 0) {
+                // Remove this item and set the head of the list
+                // as the new item
+                
+                free_item(item);
+                LinkedList* node = head;
+                head = head->next;
+                node->next = NULL;
+                table->itens[index] = create_item(node->item->key, node->item->value);
+                free_linkedlist(node);
+                table->overflow_buckets[index] = head;
+                return;
+            }
+
+            LinkedList* curr = head;
+            LinkedList* prev = NULL;
+            
+            while (curr) {
+                if (strcmp(curr->item->key, key) == 0) {
+                    if (prev == NULL) {
+                        // First element of the chain. Remove the chain
+                        free_linkedlist(head);
+                        table->overflow_buckets[index] = NULL;
+                        return;
+                    }
+                    else {
+                        // This is somewhere in the chain
+                        prev->next = curr->next;
+                        curr->next = NULL;
+                        free_linkedlist(curr);
+                        table->overflow_buckets[index] = head;
+                        return;
+                    }
+                }
+                curr = curr->next;
+                prev = curr;
+            }
+
+        }
+    }
+}
 s_info_endereco * acessa_elemento(s_hash_table * table, unsigned int key);
 
 s_hash_table * fill_table(FILE * f, int size){
